8ac325d03e7d1297e8e998d9b0dcc41e0b43a071
krmblkc <67893921+krmblkc@users.noreply.github.com>
2021-02-27 13:11:44 +0300

    My exercises with Java for Spring

---
+++ coronovirus-tracker/src/test/java/com/example/coronovirustracker/CoronovirusTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.coronovirustracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoronovirusTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
